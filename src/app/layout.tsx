f9571c4a720441502b30aee7079164935901e42e
Adrian Jazmin <adriancoderajazmin@gmail.com>
2024-10-12 18:57:14 +0800

    auth api

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { QueryProvider } from "@/components/query-provider";
 import { cn } from "@/lib/utils";
 
 const inter = Inter({subsets: ["latin"]});
@@ -21,7 +21,9 @@ export default function RootLayout({
       <body
         className={cn(inter.className, "antialiased min-h-screen")}
       >
+        <QueryProvider>
         {children}
+        </QueryProvider>
       </body>
     </html>
   );
