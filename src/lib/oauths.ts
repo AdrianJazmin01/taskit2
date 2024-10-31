0f56f49b8818fd51d48303263f4b3d20d2f5a351
Adrian Jazmin <adriancoderajazmin@gmail.com>
2024-10-31 10:35:13 +0800

    oauth fix

--- src/lib/oauths.ts
+++ src/lib/oauths.ts
@@ -7,12 +7,13 @@ import { headers } from 'next/headers';
 
 export async function signUpWithGoogle() {
   const {account} =  await createAdminClient();
-  const origin = headers().get("origin");
+
+  // const origin = headers().get("origin");
 
   const redirectUrl = await account.createOAuth2Token(
     OAuthProvider.Google,
-    `${origin}/oauth`,
-    `${origin}/sign-up`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/oauth`,
+    `${process.env.NEXT_PUBLIC_APP_URL}/sign-up`,
   );
 
   return redirect ( redirectUrl );
