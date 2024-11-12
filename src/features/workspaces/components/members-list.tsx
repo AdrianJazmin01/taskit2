d0f4ef031214459f6a0cfea3d6d8dffd3c90265d
Adrian Jazmin <adriancoderajazmin@gmail.com>
2024-11-13 06:11:40 +0800

    revert

--- src/features/workspaces/components/members-list.tsx
+++ src/features/workspaces/components/members-list.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { useWorkspaceId } from "../hooks/use-workspace-id";
 import { Button } from "@/components/ui/button";
 import { ChevronLeft, MoreVerticalIcon } from "lucide-react";
 import Link from "next/link";
-import { DottedSeparator } from "@/components/dotted-separator";  
+import { DottedSeparator } from "@/components/dotted-separator";
 import { useGetmembers } from "@/features/members/api/use-get-members";
 import { Fragment } from "react";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
@@ -16,14 +16,15 @@ import { useUpdateMember } from "@/features/members/api/use-update-member";
 import { MemberRole } from "@/features/members/types";
 import { useConfirm } from "@/hooks/use-confirm";
 
+
 export const MembersList = () => {
   const workspaceId = useWorkspaceId();
-  const { data } = useGetmembers({ workspaceId });
+  const { data } = useGetmembers({workspaceId});
   const [ConfirmDialog, confirm] = useConfirm(
     "Remove Member",
     "This Member will be removed from Workspace",
     "destructive",
-  );
+  ); 
 
   const {
     mutate: deleteMember,
@@ -35,95 +36,89 @@ export const MembersList = () => {
     isPending: isUpdatingMember 
   } = useUpdateMember();
 
-  const handleUpdateMember = (memberId: string, role: MemberRole) => {
+  const handleUpdateMember = (memberId: string, role: MemberRole) =>{
     updateMember({
       json: { role },
       param: { memberId },
-    });
-  };
+    })
+  }
 
-  const handleDeleteMember = async (memberId: string) => {
+  const handleDeleteMember = async (memberId: string) =>{
     const ok = await confirm();
-    if (!ok) return;
+    if(!ok) return;
 
     deleteMember({ param: { memberId } }, {
       onSuccess: () => {
         window.location.reload();
       },
     });
-  };
+  }
+
 
   return (
     <Card className="w-full h-full border-none shadow-xl">
-      <ConfirmDialog />
+      <ConfirmDialog/>
       <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
-        <Button asChild size="sx" variant="secondary">
-          <Link href={`/workspaces/${workspaceId}`}>
-            <ChevronLeft />
-          </Link>
+      <Button asChild size="sx" variant={"secondary"} >
+        <Link href={`/workspaces/${workspaceId}`}>
+          <ChevronLeft  />
+        </Link>
         </Button>
         <CardTitle className="text-xl font-bold">
           Members List
         </CardTitle>
       </CardHeader>
       <div className="px-7">
-        <DottedSeparator />
+        <DottedSeparator/>
       </div>
       <CardContent className="p-7">
         {data?.documents.map((member, index) => (
           <Fragment key={member.$id}>
             <div className="flex items-center gap-2">
-              <MemberAvatar
-                className="size-10"
-                fallbackClassName="text-lg"
-                name={member.name}
-              />
-              <div className="flex flex-col">
-                <p className="text-sm font-medium flex items-center gap-2">
-                  {member.name}
-                  <span className={`text-xs font-semibold ${member.role === "ADMIN" ? "text-blue-600" : "text-gray-500"}`}>
-                    {member.role === "ADMIN" ? "Admin" : "Member"}
-                  </span>
-                </p>
+              <MemberAvatar 
+              className="size-10"
+              fallbackClassName="text-lg"
+              name={member.name}
+               />
+               <div className=" flex flex-col">
+                <p className="text-sm font-medium">{member.name}</p>
                 <p className="text-xs text-muted-foreground">{member.email}</p>
-              </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    className="ml-auto"
-                    variant="secondary"
-                    size="icon"
+               </div>
+               <DropdownMenu>
+                <DropdownMenuTrigger asChild >
+
+                  <Button 
+                  className="ml-auto"
+                  variant={"secondary"}
+                  size="icon"
                   >
-                    <MoreVerticalIcon className="size-4 text-muted-foreground" />
+                    <MoreVerticalIcon  className="size-4 text-muted-foreground"/>
                   </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent side="bottom" align="end">
-                  <DropdownMenuItem
-                    className="font-medium"
+                  <DropdownMenuItem className="font-medium" 
                     onClick={() => handleUpdateMember(member.$id, MemberRole.ADMIN)}
                     disabled={isUpdatingMember}
-                  >
-                    Set as Administrator
+                    >
+                      Set as Administrator
                   </DropdownMenuItem>
-                  <DropdownMenuItem
-                    className="font-medium"
-                    onClick={() => handleUpdateMember(member.$id, MemberRole.MEMBER)}
+                  <DropdownMenuItem className="font-medium" 
+                    onClick={() => handleUpdateMember(member.$id, MemberRole.ADMIN)}
                     disabled={isUpdatingMember}
-                  >
-                    Set as Member
+                    >
+                      Set as Member
                   </DropdownMenuItem>
-                  <DropdownMenuItem
-                    className="font-medium text-amber-700"
+                  <DropdownMenuItem className="font-medium text-amber-700" 
                     onClick={() => handleDeleteMember(member.$id)}
                     disabled={isDeletingMember}
-                  >
-                    Remove {member.name}
+                    >
+                     Remove {member.name}
                   </DropdownMenuItem>
                 </DropdownMenuContent>
-              </DropdownMenu>
+               </DropdownMenu>
             </div>
             {index < data.documents.length - 1 && (
-              <Separator className="my-2.5" />
+              <Separator className="my-2.5"/>
             )}
           </Fragment>
         ))}
